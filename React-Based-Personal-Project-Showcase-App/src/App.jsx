7ba5e6f053d6612fca05eeb4becb7d1a186c465c
Your Name <your@email.com>
2026-08-14 23:02:49 +0300

    project update

--- React-Based-Personal-Project-Showcase-App/src/App.jsx
+++ React-Based-Personal-Project-Showcase-App/src/App.jsx
@@ -1,27 +1,38 @@
 import React from "react";
-//import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
 
 // Pages
 //import Landing from "./pages/Landing.jsx";
 //import AddProduct from "./pages/AddProduct.jsx";
-//import ProjectDetail from "./pages/ProjectDetail.jsx";
+//import ProductDetail from "./pages/ProductDetail.jsx";
 //import Search from "./pages/Search.jsx";
 
 // Components
 //import Navbar from "./components/Navbar.jsx";
 //import Footer from "./components/Footer.jsx";
-import ProjectCard from "./components/ProjectCard.jsx";
-import ProjectsList from "./components/ProjectsList.jsx";
-//import ProjectsForm from "./components/ProjectsForm.jsx";
+import ProductCard from "./components/ProductCard.jsx";
+import ProductsList from "./components/ProductsList.jsx";
+//import ProductsForm from "./components/ProductsForm.jsx";
 //import SearchBar from "./components/SearchBar.jsx";
-//import EditProjectForm from "./components/EditProjectsForm.jsx";
+//import EditProductForm from "./components/EditProductsForm.jsx";
 //import Loader from "./components/Loader.jsx";
 //import ErrorMessage from "./components/ErrorMessage.jsx";
-import { getBeers, getProjects, createProject, updateProject, deleteProject } from "./API/Projects.js";
- function App(){
 
-return(
+// API functions
+import { getProducts, getProduct, createProduct, updateProduct, deleteProduct } from "./API/Products.js";
 
-)
+function App() {
+  return (
+    <Router>
+      <div className="app">
+        <h1>My Product Showcase</h1>
+        <Routes>
+          <Route path="/" element={<ProductsList getProducts={getProducts} />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
 
- }
+export default App;
--- React-Based-Personal-Project-Showcase-App/src/components/ProjectsList.jsx
+++
@@ -1,6 +0,0 @@
-export default function ProjectList(){
-return(
-    <>
-    </>
-)
-}
