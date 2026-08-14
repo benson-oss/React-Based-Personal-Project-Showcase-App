95c6387d519230bbdf3c709bb60416ff3f095079
Your Name <your@email.com>
2026-08-14 11:28:39 +0300

    first commit

---
+++ React-Based-Personal-Project-Showcase-App/src/App.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// Pages
+import Landing from "./pages/Landing.jsx";
+import AddProduct from "./pages/AddProduct.jsx";
+import ProductDetail from "./pages/ProductDetail.jsx";
+import Search from "./pages/Search.jsx";
+
+// Components
+import Navbar from "./components/Navbar.jsx";
+import Footer from "./components/Footer.jsx";
+import ProductCard from "./components/ProductCard.jsx";
+import ProductList from "./components/ProductList.jsx";
+import ProductForm from "./components/ProductForm.jsx";
+import SearchBar from "./components/SearchBar.jsx";
+import EditProductForm from "./components/EditProductForm.jsx";
+import Loader from "./components/Loader.jsx";
+import ErrorMessage from "./components/ErrorMessage.jsx";
+
+ function App(){
+
+return(
+
+)
+
+ }
