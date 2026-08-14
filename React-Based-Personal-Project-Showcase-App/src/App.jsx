6ad3ade2d6bc88fd605be2198e9d14e464a63ae9
Your Name <your@email.com>
2026-08-14 21:42:09 +0300

    first commit

--- React-Based-Personal-Project-Showcase-App/src/App.jsx
+++ React-Based-Personal-Project-Showcase-App/src/App.jsx
@@ -1,33 +1,26 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // Pages
-//import Landing from "./pages/Landing.jsx";
-//import AddProduct from "./pages/AddProduct.jsx";
-//import ProjectDetail from "./pages/ProjectDetail.jsx";
-//import Search from "./pages/Search.jsx";
+import Landing from "./pages/Landing.jsx";
+import AddProduct from "./pages/AddProduct.jsx";
+import ProjectDetail from "./pages/ProjectDetail.jsx";
+import Search from "./pages/Search.jsx";
 
 // Components
-//import Navbar from "./components/Navbar.jsx";
-//import Footer from "./components/Footer.jsx";
+import Navbar from "./components/Navbar.jsx";
+import Footer from "./components/Footer.jsx";
 import ProjectCard from "./components/ProjectCard.jsx";
 import ProjectsList from "./components/ProjectsList.jsx";
-///import ProjectsForm from "./components/ProjectsForm.jsx";
-//import SearchBar from "./components/SearchBar.jsx";
-//import EditProjectForm from "./components/EditProjectsForm.jsx";
-//import Loader from "./components/Loader.jsx";
-//import ErrorMessage from "./components/ErrorMessage.jsx";
+import ProjectsForm from "./components/ProjectsForm.jsx";
+import SearchBar from "./components/SearchBar.jsx";
+import EditProjectForm from "./components/EditProjectsForm.jsx";
+import Loader from "./components/Loader.jsx";
+import ErrorMessage from "./components/ErrorMessage.jsx";
 import { getProjects, getProject, createProject, updateProject, deleteProject } from "./API/Projects.js";
  function App(){
 
 return(
-<Router>
-      <div className="app">
-        <h1>My Project Showcase</h1>   
-        <Routes>
-          <Route path="/" element={<ProjectsList getProjects={getProjects} />} />
-        </Routes>
-      </div>
-    </Router>
+
 )
 
  }
