52615d32f912c59c85b6589965c4d3964b9a6f98
Your Name <your@email.com>
2026-08-16 11:07:25 +0300

    WIP: local App.jsx changes

--- React-Based-Personal-Project-Showcase-App/src/App.jsx
+++ React-Based-Personal-Project-Showcase-App/src/App.jsx
@@ -6,6 +6,8 @@ import Landing from "./pages/Landing.jsx";
 import AddProduct from "./pages/Add.jsx"
 import ProductDetail from "./pages/ProductDetail.jsx"
 import Search from "./pages/Search.jsx"
+import ProductDetail from "./pages/ProductDetail.jsx"
+import Search from "./pages/Search.jsx";
 
 // Components
 import Navbar from "./components/ Navbar.jsx"
